8acf127144a4f06409b538c99a6fad01f29c6ae2
Marco Herrera <saedaes@gmail.com>
2013-05-23 17:31:57 -0600

    Se agrega pickerview para las prioridades

--- Gestion2013iOS/NewTaskView.cs
+++ Gestion2013iOS/NewTaskView.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Drawing;
+using System.Collections.Generic;
+
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
 
@@ -7,6 +9,8 @@ namespace Gestion2013iOS
 {
 	public partial class NewTaskView : UIViewController
 	{
+		ActionSheetPicker actionSheetPicker;
+		PickerDataModel pickerDataModel;
 		public NewTaskView () : base ("NewTaskView", null)
 		{
 			this.Title = "Nueva Tarea";
@@ -24,7 +28,106 @@ namespace Gestion2013iOS
 		{
 			base.ViewDidLoad ();
 			
-			// Perform any additional setup after loading the view, typically from a nib.
+			this.btnPrioridad.TouchUpInside += (sender, e) => {
+				actionSheetPicker.Show();
+			};
+
+			//Opciones para la lista de prioridades
+			List<String> opciones1 = new List<String>();
+			opciones1.Add ("Alta");
+			opciones1.Add ("Normal");
+			opciones1.Add ("Baja");
+
+			pickerDataModel = new PickerDataModel ();
+			pickerDataModel.Items = opciones1;
+
+			actionSheetPicker = new ActionSheetPicker(this.View);
+			actionSheetPicker.Title = "Listado de Estados";
+			actionSheetPicker.Picker.Source = pickerDataModel;
+
+			pickerDataModel.ValueChanged += (sender, e) => {
+				this.lblPrioridad.Text = pickerDataModel.SelectedItem.ToString();
+			};
+
+		}
+
+		public override void TouchesEnded (NSSet touches, UIEvent evt)
+		{
+			foreach (var item in this) {
+				var tf = item as UITextField;
+				if (tf != null && tf.IsFirstResponder) {
+					tf.ResignFirstResponder ();
+				}
+			}
+			base.TouchesEnded (touches, evt);
+		}
+
+		/** Clase para manejar el picker **/
+		protected class PickerDataModel : UIPickerViewModel 
+		{
+			public event EventHandler<EventArgs> ValueChanged;
+
+			/// <summary>
+			/// The items to show up in the picker
+			/// </summary>
+			public List<String> Items
+			{
+				get { return items; }
+				set { items = value; }
+			}
+			List<String> items = new List<String>();
+
+			/// <summary>
+			/// The current selected item
+			/// </summary>
+			public String SelectedItem
+			{
+				get { return items[selectedIndex]; }
+			}
+			protected int selectedIndex = 0;
+
+			/// <summary>
+			/// default constructor
+			/// </summary>
+			public PickerDataModel ()
+			{
+			}
+
+			/// <summary>
+			/// Called by the picker to determine how many rows are in a given spinner item
+			/// </summary>
+			public override int GetRowsInComponent (UIPickerView picker, int component)
+			{
+				return items.Count;
+			}
+
+			/// <summary>
+			/// called by the picker to get the text for a particular row in a particular 
+			/// spinner item
+			/// </summary>
+			public override string GetTitle (UIPickerView picker, int row, int component){
+				return items[row].ToString();
+			}
+
+			/// <summary>
+			/// called by the picker to get the number of spinner items
+			/// </summary>
+			public override int GetComponentCount (UIPickerView picker)
+			{
+				return 1;
+			}
+
+			/// <summary>
+			/// called when a row is selected in the spinner
+			/// </summary>
+			public override void Selected (UIPickerView picker, int row, int component)
+			{
+				selectedIndex = row;
+				if (this.ValueChanged != null)
+				{
+					this.ValueChanged (this, new EventArgs ());
+				}	
+			}
 		}
 	}
 }
--- Gestion2013iOS/NewTaskView.designer.cs
+++ Gestion2013iOS/NewTaskView.designer.cs
@@ -1,6 +1,6 @@
 // WARNING
 //
-// This file has been generated automatically by MonoDevelop to store outlets and
+// This file has been generated automatically by Xamarin Studio to store outlets and
 // actions made in the Xcode designer. If it is removed, they will be lost.
 // Manual changes to this file may not be handled correctly.
 //
@@ -11,8 +11,55 @@ namespace Gestion2013iOS
 	[Register ("NewTaskView")]
 	partial class NewTaskView
 	{
+		[Outlet]
+		MonoTouch.UIKit.UIButton btnPrioridad { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UIButton btnFechaCont { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UIButton btnFechaCompr { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UILabel lblPrioridad { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UILabel lblFechaCont { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UILabel lblFechaCompr { get; set; }
+		
 		void ReleaseDesignerOutlets ()
 		{
+			if (btnPrioridad != null) {
+				btnPrioridad.Dispose ();
+				btnPrioridad = null;
+			}
+
+			if (btnFechaCont != null) {
+				btnFechaCont.Dispose ();
+				btnFechaCont = null;
+			}
+
+			if (btnFechaCompr != null) {
+				btnFechaCompr.Dispose ();
+				btnFechaCompr = null;
+			}
+
+			if (lblPrioridad != null) {
+				lblPrioridad.Dispose ();
+				lblPrioridad = null;
+			}
+
+			if (lblFechaCont != null) {
+				lblFechaCont.Dispose ();
+				lblFechaCont = null;
+			}
+
+			if (lblFechaCompr != null) {
+				lblFechaCompr.Dispose ();
+				lblFechaCompr = null;
+			}
 		}
 	}
 }
