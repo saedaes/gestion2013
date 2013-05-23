a9209264d855a2a938618bc0edd12152826b9a88
Marco Herrera <saedaes@gmail.com>
2013-05-23 15:21:57 -0600

    Se corrige un error menor

---
+++ Gestion2013iOS/NewTaskView.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Drawing;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace Gestion2013iOS
+{
+	public partial class NewTaskView : UIViewController
+	{
+		public NewTaskView () : base ("NewTaskView", null)
+		{
+			this.Title = "Nueva Tarea";
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning ();
+			
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+	}
+}
+
---
+++ Gestion2013iOS/obj/Xcode/4/TasksView.m
@@ -0,0 +1,17 @@
+// WARNING
+// This file has been generated automatically by Xamarin Studio to
+// mirror C# types. Changes in this file made by drag-connecting
+// from the UI designer will be synchronized back to C#, but
+// more complex manual changes may not transfer correctly.
+
+
+#import "TasksView.h"
+
+@implementation TasksView
+
+@synthesize tblTasks = _tblTasks;
+@synthesize headerView = _headerView;
+@synthesize btnNuevo = _btnNuevo;
+@synthesize btnMap = _btnMap;
+
+@end
